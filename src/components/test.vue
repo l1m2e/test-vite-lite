4128ccf30cffc30b6fce34f4f3f653e26f7bf370
l1m2e <1956227985@qq.com>
2023-08-04 19:44:40 +0800

    'init'

---
+++ src/components/test.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import type { TestEmits } from './test'
+
+const emits = defineEmits<TestEmits>()
+</script>
+
+<template>
+  <div btn @click="emits('go')">
+    点击我会有警告在控制台
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+</script>
+
+<template>
+  <test />
+</template>
